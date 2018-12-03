cba57728a25c12213387d0b46282c1eb161d8f0f
AsyrafN <asyrafnaqi@yahoo.com>
2018-12-03 13:51:14 +0800

    Added Logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  delete "/sign_out" => "users#destroy", as: "sign_out"
   get "/sign_in" => "users#sign_in", as: "sign_in"
   post "/login" => "users#login", as: "login"
 
